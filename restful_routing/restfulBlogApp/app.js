b1cbbc17fbba28e544935d6755efd31a4ae4a9dc
niknows <33270711+niknows@users.noreply.github.com>
2018-07-14 22:17:13 -0300

    Add files via upload

--- restful_routing/restfulBlogApp/app.js
+++ restful_routing/restfulBlogApp/app.js
@@ -1,14 +1,18 @@
-var express = require("express");
-var app = express();
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
+/*BASIC SETUP*/
+var express     = require("express");
+var mongoose    = require("mongoose");
+var bodyParser  = require("body-parser");
+var app         = express();
 
-mongoose.connect("",{useNewUrlParser: true});
+/*CONNECTING PACKAGES*/
+mongoose.connect("mongodb://localhost:27017//restful_blog_app",{useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 
+/*SERVER*/
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Blog App has started...");
-})
+});
 
